ba81fb5f0e89c8cb9318a29f59e3b79d400e1936
salvadorvanoli <141060855+salvadorvanoli@users.noreply.github.com>
2023-08-06 14:27:20 -0300

    Agrego js y funcionalidad a un div

---
+++ eventos.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+let div = document.getElementsByTagName("div")[0];
+
+div.addEventListener("click", function(){
+    alert('Hola! Soy el div');
+});
+
+});
